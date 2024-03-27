7a1857ebfef5a6858afd45d250a63d15b67e9207
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2024-03-27 08:36:30 +0000

    feat: multiple test cases added for Subtract()

--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -25,10 +25,21 @@ func TestAdd(t *testing.T) {
 }
 func TestSubtract(t *testing.T) {
 	t.Parallel()
-	var want float64 = 2
-	got := calculator.Subtract(4, 2)
-	if want != got {
-		t.Errorf("want %f, got %f", want, got)
+	type testCase struct {
+		a, b float64
+		want float64
+	}
+	testCases := []testCase{
+		{a: 4, b: 2, want: 2},
+		{a: 2, b: 4, want: -2},
+		{a: -2, b: -2, want: 0},
+		{a: 3.5, b: 0.2, want: 3.3},
+	}
+	for _, tc := range testCases {
+		got := calculator.Subtract(tc.a, tc.b)
+		if tc.want != got {
+			t.Errorf("Subtract(%f, %f): want %f, got %f", tc.a, tc.b, tc.want, got)
+		}
 	}
 }
 
