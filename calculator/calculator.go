787dc2ee0f9c95060858d6c582147caaad8791d6
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2024-03-27 09:55:52 +0000

    feat: error handling for diving by zero

--- calculator/calculator.go
+++ calculator/calculator.go
@@ -1,6 +1,8 @@
 // Package calculator does simple calculations.
 package calculator
 
+import "errors"
+
 // Add takes two numbers and returns the result of adding
 // them together.
 func Add(a, b float64) float64 {
@@ -13,4 +15,10 @@ func Subtract(a, b float64) float64 {
 
 func Multiply(a, b float64) float64 {
 	return a * b
+}
+func Divide(a, b float64) (float64, error) {
+	if b == 0 {
+		return 0, errors.New("division by zero not allowed")
+	}
+	return a / b, nil
 }
