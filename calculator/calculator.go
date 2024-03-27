7f8320c5bdd433da7656ce0a177ffcf7116107a4
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2024-03-27 07:23:05 +0000

    calculator project init

---
+++ calculator/calculator.go
@@ -0,0 +1,8 @@
+// Package calculator does simple calculations.
+package calculator
+
+// Add takes two numbers and returns the result of adding
+// them together.
+func Add(a, b float64) float64 {
+	return a + b
+}
