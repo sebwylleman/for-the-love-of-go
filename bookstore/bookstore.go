8ea1b8a61dfbf31e9288d9f0c6fa61045a7cc468
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2024-03-28 12:05:04 +0000

    feat: Buy function declared and passed the first test

--- bookstore/bookstore.go
+++ bookstore/bookstore.go
@@ -5,3 +5,11 @@ type Book struct {
 	Author string
 	Copies int
 }
+
+func Buy(b1 Book) Book {
+	return Book{
+		Title:  b1.Title,
+		Author: b1.Author,
+		Copies: b1.Copies - 1,
+	}
+}
--- bookstore/bookstore_test.go
+++ bookstore/bookstore_test.go
@@ -13,3 +13,18 @@ func TestBook(t *testing.T) {
 		Copies: 2,
 	}
 }
+
+func TestBuy(t *testing.T) {
+	t.Parallel()
+	b := bookstore.Book{
+		Title:  "Dune",
+		Author: "F. Herbert",
+		Copies: 10,
+	}
+	want := 9
+	result := bookstore.Buy(b)
+	got := result.Copies
+	if got != want {
+		t.Errorf("got %d copies, want %d", got, want)
+	}
+}
